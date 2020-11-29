4c9f6e7f54c71ef6b231fe9ff4d57bcf141d2f09
Will Ryan <will.ryan@gluwa.com>
2020-11-29 00:41:59 -0800

    Force my provider

--- src/utils/web3-loader.js
+++ src/utils/web3-loader.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3'
 
-const FALLBACK_PROVIDER = "http://localhost:8545"
+const PROVIDER = "http://baas-rpc.luniverse.io:8545?lChainId=3158073271666164067"
 
 let getWeb3 = new Promise(function(resolve, reject) {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
@@ -9,20 +9,7 @@ let getWeb3 = new Promise(function(resolve, reject) {
     var web3 = window.web3
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-      // Use Mist/MetaMask's provider.
-      web3 = new Web3(web3.currentProvider)
-
-      results = {
-        web3: web3
-      }
-
-      console.log('Injected web3 detected.');
-
-      resolve(results)
-    } else {
-      // Fallback to localhost if no web3 injection.
-      var provider = new Web3.providers.HttpProvider(FALLBACK_PROVIDER)
+      var provider = new Web3.providers.HttpProvider(PROVIDER)
 
       web3 = new Web3(provider)
 
@@ -33,7 +20,6 @@ let getWeb3 = new Promise(function(resolve, reject) {
       console.log('No web3 instance injected, using Local web3.');
 
       resolve(results)
-    }
   })
 })
 
